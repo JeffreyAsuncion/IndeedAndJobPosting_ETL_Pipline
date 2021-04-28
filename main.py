aba241266db1b9cafefe05dc7648754c44bb9094
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-28 18:18:50 -0400

    soup.find_all() is good but iterating gives none

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from bs4 import BeautifulSoup
+import lxml
 
 with open('home.html', 'r') as html_file:   # r is read only
     content = html_file.read()
-    print(content)
+    # print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    # find h5 tags
+    # tags = soup.find('h5')       # finds first one
+    # print(tag)
+
+    courses_html_tags = soup.find_all('h5')
+    for course in courses_html_tags:
+
+        print(course)
+        # print(type(course))
