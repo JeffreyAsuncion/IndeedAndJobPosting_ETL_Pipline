3c2ad73155d4aa32f02e04b393b5f473db0ff6aa
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-30 08:23:30 -0400

    date_pulled implemented in script

--- JobPost008.py
+++ JobPost008.py
@@ -5,6 +5,7 @@ import requests
 import time
 import sqlite3
 from Jobs import Job
+from datetime import datetime
 
 # Next steps try with multiple unfamiliar skills
 print("Put some skill that you are not familiar with.") 
@@ -29,24 +30,26 @@ def find_jobs():
         published_date = job.find('span', class_ = 'sim-posted').span.text
 
         if 'few' in published_date:
-            
+            # clean up scraped data            
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')      #replace is to clean the whitespaces
             skills = job.find('span', class_='srp-skills').text.replace(' ','')
             more_info = job.header.h2.a['href']
             
             # add class
+            date_pulled = datetime.now()
             post = company_name.strip()
             description = skills.strip()
             link = more_info
-            J = Job(post, description, link)
+            J = Job(date_pulled, post, description, link)
 
 
             if unfamiliar_skill not in skills:
+                print(f"Date pulled: {J.date_pulled}")
                 print(f"Company Name: {J.post}") 
                 print(f"Required Skills: {J.description}")
                 print(f"More Info: {J.link}")
                 print("~" * 20)
-                cursor.execute("INSERT INTO jobslist VALUES(?, ?, ?)" , (J.post, J.description, J.link))
+                cursor.execute("INSERT INTO jobslist VALUES(?, ?, ?, ?)" , (J.date_pulled, J.post, J.description, J.link))
                 conn.commit()
     
     conn.close()
--- Jobs.py
+++ Jobs.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 class Job:
 
     def __init__(self, date_pulled, post, description, link):
