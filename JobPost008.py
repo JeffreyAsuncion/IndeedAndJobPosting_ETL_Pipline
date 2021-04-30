73cbefc5c72404248e33d16c42ad3aaa4702009f
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-30 08:16:27 -0400

    Job class new attribute date pulled

--- JobPost008.py
+++ JobPost008.py
@@ -1,3 +1,5 @@
+# add timestamp
+
 from bs4 import BeautifulSoup
 import requests
 import time
--- Jobs.py
+++ Jobs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class Job:
 
     def __init__(self, date_pulled, post, description, link):
-        self.date_pulled = datetime.now()
+        self.date_pulled = date_pulled
         self.post = post
         self.description = description
         self.link = link
