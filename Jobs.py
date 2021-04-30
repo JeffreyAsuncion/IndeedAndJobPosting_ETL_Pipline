3a0406e80bdd5dd905bf01805f16e61f2561be91
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-30 08:15:00 -0400

    Job class new attribute date pulled

--- Jobs.py
+++ Jobs.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 class Job:
 
-    def __init__(self, post, description, link):
+    def __init__(self, date_pulled, post, description, link):
+        self.date_pulled = datetime.now()
         self.post = post
         self.description = description
         self.link = link
-        
