6eddcf90afefd20906fc75be5337d1c2ac689c9d
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-01 16:26:27 -0400

    scrape jobs and insert into database

--- JobPost006.py
+++ JobPost006.py
@@ -5,6 +5,7 @@ import sqlite3
 
 
 # Next steps try with multiple unfamiliar skills
+
 print("Put some skill that you are not familiar with.") 
 unfamiliar_skill = input("> ") 
 print(f"Filtering out {unfamiliar_skill}")
@@ -15,9 +16,7 @@ def find_jobs():
     # Create cursor
     cursor = conn.cursor()
 
-
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-    # print(html_text)
 
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
