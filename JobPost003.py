607ac704fffa64de8a6d965190ed9ff5b487f0bd
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-01 16:13:30 -0400

    scrape most recent jobs excluding those with an unfamiliar skill

--- JobPost003.py
+++ JobPost003.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-
 # Next steps try with multiple unfamiliar skills
+
 print("Put some skill that you are not familiar with.") 
 unfamiliar_skill = input("> ") 
 print(f"Filtering out {unfamiliar_skill}")
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-# print(html_text)
+
 
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
@@ -24,6 +23,7 @@ for job in jobs:
         skills = job.find('span', class_='srp-skills').text.replace(' ','')
         more_info = job.header.h2.a['href']
 
+        # filter out unfamilar skills
         if unfamiliar_skill not in skills:
             print(f"Company Name: {company_name.strip()}") 
             print(f"Required Skills: {skills.strip()}")
