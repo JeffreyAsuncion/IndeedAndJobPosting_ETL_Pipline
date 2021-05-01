5a06e67f592bfb3dc4b19e0028c589723a7af48a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-01 16:21:34 -0400

    automate and scrape every 10 minutes

--- JobPost004.py
+++ JobPost004.py
@@ -10,7 +10,7 @@ print(f"Filtering out {unfamiliar_skill}")
 
 def find_jobs():
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-    # print(html_text)
+
 
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
